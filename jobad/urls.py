39c2cb2ff6c84ac48d6c2b1ab33c40c872a36a0d
cybergod96 <cybergod96@bitbucket.org>
2018-04-23 12:25:48 +0200

    Added templates

--- jobad/urls.py
+++ jobad/urls.py
@@ -5,4 +5,8 @@ app_name = 'jobad'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ad/<int:id>', views.viewad, name='viewad'),
+    path('apply/<int:id>', views.appply, name='apply'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
--- jobad/views.py
+++ jobad/views.py
@@ -3,4 +3,21 @@ from django.http import HttpResponse
 
 
 def index(request):
+    ads = dict()
+    return render(request, 'jobad/home.html', ads)
+
+
+def viewad(request, ad_id):
+    return HttpResponse(request, "Not implemented yet.")
+
+
+def appply(request, ad_id):
+    return HttpResponse(request, "Not implemented yet.")
+
+
+def register(request):
+    return HttpResponse(request, "Not implemented yet.")
+
+
+def login(request):
     return HttpResponse(request, "Not implemented yet.")
